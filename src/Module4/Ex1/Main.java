dbf62fad4638a937426cbdeb1093d8dcf351fd70
Александр <stepushins@mail.ru>
2025-09-28 05:29:04 +0300

    Module4- Реализация примера DeadLock

--- src/Module4/Ex1/Main.java
+++ src/Module4/Ex1/Main.java
@@ -3,5 +3,22 @@ package Module4.Ex1;
 public class Main {
     public static void main(String[] args) {
 
+        DeadLockExample deadLockExample = new DeadLockExample();
+
+        Thread DeadLockthread1 = new Thread(()->{
+           while (true){
+               deadLockExample.method1();
+           }
+        });
+
+        Thread DeadLockthread2 = new Thread(()->{
+           while (true){
+               deadLockExample.method2();
+           }
+        });
+
+        DeadLockthread1.start();
+        DeadLockthread2.start();
+
     }
 }
